0b2e68a2b0d2583e9073351568f67faf41a58991
Marco <marcoabustam@gmail.com>
2024-11-25 19:31:11 -0300

    fix problem with array Empty v.2

--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Author } from '../schema/author.schema';
+import { Author, AuthorDocument } from '../schema/author.schema';
 import { CreateAuthorDto } from '../dto/create-author.dto';
 
 @Injectable()
 export class AuthorsService {
   constructor(
-    @InjectModel(Author.name) private authorModel: Model<Author>,
+    @InjectModel(Author.name) private authorModel: Model<AuthorDocument>,
   ) {}
 
-  async create(createAuthorDto: CreateAuthorDto): Promise<Author> {
+  async create(createAuthorDto: CreateAuthorDto): Promise<AuthorDocument> {
     const createdAuthor = new this.authorModel(createAuthorDto);
     return createdAuthor.save();
   }
 
-  async findAll(): Promise<Author[]> {
+  async findAll(): Promise<AuthorDocument[]> {
     return this.authorModel.find().populate('books').exec();
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,32 +1,70 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Book } from '../schema/book.schema';
+import { Model, Types } from 'mongoose';
+import { Book, BookDocument } from '../schema/book.schema';
+import { Author, AuthorDocument } from '../schema/author.schema';
 import { CreateBookDto } from '../dto/create-book.dto';
 
 @Injectable()
 export class BooksService {
-
   constructor(
-    @InjectModel(Book.name) private bookModel: Model<Book>,) {}
+    @InjectModel(Book.name) private bookModel: Model<BookDocument>,
+    @InjectModel(Author.name) private authorModel: Model<AuthorDocument>,
+  ) {}
 
-  async create(createBookDto: CreateBookDto): Promise<Book> {
-    const createdBook = new this.bookModel(createBookDto);
-    return createdBook.save();
+  async create(createBookDto: CreateBookDto): Promise<BookDocument> {
+    // Convertir string IDs a ObjectIds
+    const authorObjectIds = createBookDto.authorIds.map(id => new Types.ObjectId(id));
+
+    // Verificar que todos los autores existan
+    const authors = await this.authorModel.find({
+      _id: { $in: authorObjectIds }
+    }).exec();
+
+    if (authors.length !== createBookDto.authorIds.length) {
+      throw new NotFoundException('Uno o más autores no fueron encontrados');
+    }
+
+    const createdBook = new this.bookModel({
+      title: createBookDto.title,
+      chapters: createBookDto.chapters,
+      pages: createBookDto.pages,
+      authors: authorObjectIds
+    });
+
+    const savedBook = await createdBook.save();
+
+    // Actualizar los autores con el nuevo libro
+    await Promise.all(
+      authors.map(async (author) => {
+        return this.authorModel.findByIdAndUpdate(
+          author._id,
+          {
+            $push: { books: savedBook._id }
+          },
+          { new: true }
+        );
+      })
+    );
+
+    return this.bookModel
+      .findById(savedBook._id)
+      .populate('authors')
+      .exec();
   }
 
-  async findAll(): Promise<Book[]> {
-    return this.bookModel.find().populate('authors').exec();
-  }
-
-  async findBookById(id: string){
-    return this.bookModel.findById(id).exec();
+  async findAll(): Promise<BookDocument[]> {
+    return this.bookModel
+      .find()
+      .populate('authors', 'name')
+      .exec();
   }
 
   async getPagesPerChapter(): Promise<any[]> {
     const books = await this.bookModel.find().exec();
     return books.map(book => ({
       id: book._id,
+      title: book.title,
       averagePages: Number((book.pages / book.chapters).toFixed(2))
     }));
   }
