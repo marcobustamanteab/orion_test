bf50283b56a5768f50c9ccc776be840cf231c00c
Marco <marcoabustam@gmail.com>
2024-11-25 19:18:18 -0300

    auth controller added, now test apis and apply fix if apply

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { AuthorsService } from './authors.service';
+import { CreateAuthorDto } from '../dto/create-author.dto';
 
 @Controller('authors')
-export class AuthorsController {}
+export class AuthorsController {
+  constructor(private readonly authorsService: AuthorsService) {}
+
+  @Post()
+  async create(@Body() createAuthorDto: CreateAuthorDto) {
+    return this.authorsService.create(createAuthorDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.authorsService.findAll();
+  }
+}
