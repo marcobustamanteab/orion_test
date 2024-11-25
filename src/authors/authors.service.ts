239ea8c092e0b36c4a8c6192df1cb85eab2917e5
Marco <marcoabustam@gmail.com>
2024-11-25 18:52:55 -0300

    author.service.ts

--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Author } from '../schema/author.schema';
+import { CreateAuthorDto } from '../dto/create-author.dto';
 
 @Injectable()
-export class AuthorsService {}
+export class AuthorsService {
+  constructor(
+    @InjectModel(Author.name) private authorModel: Model<Author>,
+  ) {}
+
+  async create(createAuthorDto: CreateAuthorDto): Promise<Author> {
+    const createdAuthor = new this.authorModel(createAuthorDto);
+    return createdAuthor.save();
+  }
+
+  async findAll(): Promise<Author[]> {
+    return this.authorModel.find().populate('books').exec();
+  }
+}
