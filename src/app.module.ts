7aa1a137fa0ac978ef140e64201a60a584c2f301
Marco <marcoabustam@gmail.com>
2024-11-25 18:19:20 -0300

    added service,controller and books module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://marcoabustam:2UDJc61TIPSGeOEd@cluster0.u0e9f.mongodb.net/oriondb_test?retryWrites=true&w=majority&appName=Cluster0')
+    MongooseModule.forRoot('mongodb+srv://marcoabustam:2UDJc61TIPSGeOEd@cluster0.u0e9f.mongodb.net/oriondb_test?retryWrites=true&w=majority&appName=Cluster0'),
+    BooksModule
   ],
   controllers: [AppController],
   providers: [AppService],
