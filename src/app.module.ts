4a9256447eb18d0e6999625aec0a0700ecfca714
Marco <marcoabustam@gmail.com>
2024-11-25 19:59:12 -0300

    comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,10 @@ import { AuthorsModule } from './authors/authors.module';
 
 @Module({
   imports: [
+
+    // en este punto, tengo mis credenciales para acceder a mi cluster de mongo atlas y a mi db. Por ahora, creo que debiese crear un archivo .env para que no se 
+    // visualicen las credenciales, claramente, pero a modo de testing y para enviar la prueba, los dejaré seteados aqui. Eventualmente, habrá que cambiarlos para 
+    // testear el código.
     MongooseModule.forRoot('mongodb+srv://marcoabustam:2UDJc61TIPSGeOEd@cluster0.u0e9f.mongodb.net/oriondb_test?retryWrites=true&w=majority&appName=Cluster0'),
     BooksModule,
     AuthorsModule,
